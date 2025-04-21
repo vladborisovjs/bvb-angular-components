3e02dad48923a270dce874de4405bc7ef6689bce
Vlad Borisov <vladborisovjs@gmail.com>
2025-04-21 18:31:42 +0300

    landing overview card

--- projects/demo-app/src/app/pages/overview/overview.component.ts
+++ projects/demo-app/src/app/pages/overview/overview.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavItems } from '../../../shared/nav-items';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'bvb-overview',
-  imports: [],
+  imports: [RouterModule,],
   standalone: true,
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OverviewComponent {
-
+  navItems = NavItems;
 }
--- projects/demo-app/src/shared/nav-items.ts
+++ projects/demo-app/src/shared/nav-items.ts
@@ -1,8 +1,13 @@
 export interface NavItem {
   path: string;
   label: string;
+  description?: string;
 }
 
 export const NavItems: NavItem[] = [
-  { path: 'components/tooltip', label: 'Tooltip' },
+  {
+    path: '/components/tooltip',
+    label: 'Tooltip',
+    description: 'Displays floating content when an object is hovered.',
+  },
 ];
